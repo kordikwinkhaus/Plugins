b8c6f8367326871510410cbee3b592209ac85a5a
Tomas Kordik <kordik@winkhaus.cz>
2016-01-29 17:33:51 +0100

    Přidán plugin tester

---
+++ PluginTester/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+using UserExtensions;
+
+namespace PluginTester
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length < 3) Environment.Exit(1);
+
+            string dllName = args[1];
+            if (!Path.IsPathRooted(dllName))
+            {
+                dllName = Path.Combine(Directory.GetCurrentDirectory(), dllName);
+            }
+            txtConnString.Text = args[2];
+
+            IExtensionsFactory fact = GetFactory(dllName);
+            if (fact != null)
+            {
+                CreatePages(fact);
+            }
+        }
+
+        private IExtensionsFactory GetFactory(string dllName)
+        {
+            try
+            {
+                Assembly asm = Assembly.LoadFile(dllName);
+                var factType = asm.GetType("UserExt.ExtensionsFactory");
+                IExtensionsFactory fact = (IExtensionsFactory)Activator.CreateInstance(factType);
+                return fact;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Cannot load tested dll: " + ex.Message, this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        private void CreatePages(IExtensionsFactory fact)
+        {
+            var vals = Enum.GetValues(typeof(EPropPage));
+
+            foreach (var val in vals)
+            {
+                try
+                {
+                    FrameworkElement page = fact.GetPropertyPage((EPropPage)val, txtConnString.Text, CultureInfo.CurrentUICulture.Name);
+                    if (page != null)
+                    {
+                        ListBoxItem item = new ListBoxItem();
+                        item.Content = page.GetType().Name;
+                        item.Tag = page;
+
+                        lstPages.Items.Add(item);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("Cannot get page for {0}: ", val) + ex.Message, this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstPages.SelectedItem != null)
+            {
+                ListBoxItem item = (ListBoxItem)lstPages.SelectedItem;
+
+                TestWindow win = item.Tag as TestWindow;
+                if (win != null)
+                {
+                    if (win.IsVisible)
+                    {
+                        win.Activate();
+                    }
+                    else
+                    {
+                        win.Show();
+                    }
+                }
+                else
+                {
+                    FrameworkElement page = (FrameworkElement)item.Tag;
+                    win = new TestWindow();
+                    win.SetTestPage(page);
+                    win.Owner = this;
+                    item.Tag = win;
+                    win.Show();
+                }
+            }
+        }
+    }
+}
---
+++ PluginTester/TestWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Xml.Linq;
+using UserExtensions;
+
+namespace PluginTester
+{
+    public partial class TestWindow : Window
+    {
+        public TestWindow()
+        {
+            InitializeComponent();
+
+            testContainer.AddHandler(MouseUpEvent, new MouseButtonEventHandler(testContainer_MouseUp), true);
+            testContainer.AddHandler(KeyUpEvent, new KeyEventHandler(testContainer_KeyUp), true);
+        }
+
+        private void testContainer_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            RefreshXmlText();
+        }
+
+        private void testContainer_KeyUp(object sender, KeyEventArgs e)
+        {
+            RefreshXmlText();
+        }
+
+        private void RefreshXmlText()
+        {
+            if (!txtUserData.IsFocused)
+            {
+                txtUserData.Text = _userdata.ToString();
+            }
+            UpdateObjectDataText();
+        }
+
+        private void UpdateObjectDataText()
+        {
+            if (_userform != null && _userform.ObjectData != null)
+            {
+                txtObjectData.Text = _userform.ObjectData.ToString();
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private XElement _userdata;
+        private IUserForm _userform;
+
+        public void SetTestPage(FrameworkElement elem)
+        {
+            this.Title += elem.GetType().Name;
+
+            _userdata = new XElement("UserData");
+            testContainer.Children.Add(elem);
+
+            _userform = elem as IUserForm;
+            if (_userform != null)
+            {
+                _userform.SetData(_userdata, 1, 1, 1);
+            }
+
+            RefreshXmlText();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (_userform != null)
+            {
+                try
+                {
+                    TrySetData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, this.Title, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void TrySetData()
+        {
+            XElement userdata = XElement.Parse(txtUserData.Text);
+            _userform.SetData(userdata, 1, 1, 1);
+            _userdata = userdata;
+
+            RefreshXmlText();
+        }
+    }
+}
