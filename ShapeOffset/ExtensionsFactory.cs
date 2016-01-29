751782cf55c613e1f7e037856703038ac5baa10c
Tomas Kordik <kordik@winkhaus.cz>
2016-01-29 18:12:42 +0100

    Základ pluginu pro otvory

---
+++ ShapeOffset/ExtensionsFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using ShapeOffset;
+using UserExtensions;
+
+namespace UserExt
+{
+    public class ExtensionsFactory : IExtensionsFactory
+    {
+        public FrameworkElement GetPropertyPage(EPropPage pg, string connection, string lang)
+        {
+            switch (pg)
+            {
+                case EPropPage.pDziura:
+                    return new ShapeOffsetPage();
+            }
+
+            return null;
+        }
+    }
+}
