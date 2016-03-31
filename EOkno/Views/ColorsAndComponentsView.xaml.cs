4045cc4942396997dd41afe205c8c6b9fe35934b
Tomas Kordik <kordik@winkhaus.cz>
2016-03-31 22:16:40 +0200

    EOkno: zalamování komponent ve vlastnostech dokumentu

--- EOkno/Views/ColorsAndComponentsView.xaml.cs
+++ EOkno/Views/ColorsAndComponentsView.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EOkno.Views
 {
-    /// <summary>
-    /// Interaction logic for ColorsAndComponentsView.xaml
-    /// </summary>
     public partial class ColorsAndComponentsView : UserControl
     {
         public ColorsAndComponentsView()
         {
             InitializeComponent();
         }
+
+        public ScrollBarVisibility ComponentsScrollbar
+        {
+            get { return (ScrollBarVisibility)GetValue(ComponentsScrollbarProperty); }
+            set { SetValue(ComponentsScrollbarProperty, value); }
+        }
+
+        public static readonly DependencyProperty ComponentsScrollbarProperty =
+            DependencyProperty.Register("ComponentsScrollbar", typeof(ScrollBarVisibility), 
+            typeof(ColorsAndComponentsView), new PropertyMetadata(ScrollBarVisibility.Disabled));
     }
 }
