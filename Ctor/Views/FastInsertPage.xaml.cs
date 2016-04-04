3cfab75e211ebf5af73a42c21ac3b8ccdb9577f8
Tomas Kordik <kordik@winkhaus.cz>
2016-04-04 13:29:54 +0200

    Skripty pro základní typy

--- Ctor/Views/FastInsertPage.xaml.cs
+++ Ctor/Views/FastInsertPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using System.Xml.Linq;
 using Ctor.ViewModels;
 using UserExtensions;
@@ -51,6 +52,206 @@ namespace Ctor.Views
         private void Mirror_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             _viewModel.RunCommand.Execute("if pos.IsConstruction: pos.Mirror()");
+        }
+
+        private void Fix_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 300:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 300:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+
+    frame = area.InsertFrame(typeID, colorID)
+    frame.InsertGlasspackets(glasspacket)";
+        }
+
+        private void FixVS_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FixS("Vertical");
+        }
+
+        private void FixHS_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            FixS("Horizontal");
+        }
+
+        private void FixS(string dir)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 300:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 300:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+
+    frame = area.InsertFrame(typeID, colorID)
+    frame.Insert" + dir + @"Mullion('14842')
+    frame.InsertGlasspackets(glasspacket)";
+        }
+
+        private void _1kl_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _1kl("False");
+        }
+
+        private void _1kp_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _1kl("True");
+        }
+
+        private void _1kl(string dir)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 400:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 500:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+    fittingsGroup = ctx.FittingsGroup
+
+    frame = area.InsertFrame(typeID, colorID)
+    sash = frame.InsertSash()
+    sash.InsertGlasspackets(glasspacket)
+    if ctx.InsertFittings:
+        args = sash.GetFittingsFindArgs()
+        fittingsTypeID = db.FindFittingsType(fittingsGroup, args)
+        sash.InsertFittings(fittingsTypeID, " + dir + ")";
+        }
+
+        private void _1ks_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 400:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 500:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+    fittingsGroup = ctx.FittingsGroup
+
+    frame = area.InsertFrame(typeID, colorID)
+    sash = frame.InsertSash()
+    sash.InsertGlasspackets(glasspacket)
+    if ctx.InsertFittings:
+        args = sash.GetFittingsFindArgs()
+        args.TiltOnly = True
+        fittingsTypeID = db.FindFittingsType(fittingsGroup, args)
+        sash.InsertFittings(fittingsTypeID)";
+        }
+
+        private void _2kl_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _2kl("True");
+        }
+
+        private void _2kp_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _2kl("False");
+        }
+
+        private void _2kl(string dir)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 900:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 500:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+    type = db.GetWindowType(typeID)
+    fittingsGroup = ctx.FittingsGroup
+
+    frame = area.InsertFrame(typeID, colorID)
+    frame.InsertFalseMullion(type.DefaultFalseMullion, " + dir + @")
+    frame.InsertGlasspackets(glasspacket)
+    if ctx.InsertFittings:
+        for sash in frame.GetSashes():
+            args = sash.GetFittingsFindArgs()
+            fittingsTypeID = db.FindFittingsType(fittingsGroup, args)
+            sash.InsertFittings(fittingsTypeID)";
+        }
+
+        private void _3kl_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 1200:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 500:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+    type = db.GetWindowType(typeID)
+    fittingsGroup = ctx.FittingsGroup
+
+    frame = area.InsertFrame(typeID, colorID)
+    x = frame.Right - 200;
+    y = (frame.Top + frame.Bottom) / 2.0
+    frame.InsertVerticalMullion('14842', 0.3333)
+    area = frame.GetArea(x, y)
+    area.InsertFalseMullion(type.DefaultFalseMullion, False)
+    frame.InsertSashes()
+    frame.InsertGlasspackets(glasspacket)
+    if ctx.InsertFittings:
+        for sash in frame.GetSashes():
+            args = sash.GetFittingsFindArgs()
+            fittingsTypeID = db.FindFittingsType(fittingsGroup, args)
+            sash.InsertFittings(fittingsTypeID)";
+        }
+
+        private void _3kp_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            txtCode.Text = @"if pos.IsConstruction:
+    area = pos.GetEmptyArea()
+    if area.Width < 1200:
+        msg.Fail('Oblast pro okno je příliš úzká.')
+    if area.Height < 500:
+        msg.Fail('Oblast pro okno je příliš nízká.')
+
+    typeID = ctx.WindowsType
+    colorID = ctx.WindowsColor
+    glasspacket = ctx.Glasspacket
+    type = db.GetWindowType(typeID)
+    fittingsGroup = ctx.FittingsGroup
+
+    frame = area.InsertFrame(typeID, colorID)
+    x = frame.Left + 200;
+    y = (frame.Top + frame.Bottom) / 2.0
+    frame.InsertVerticalMullion('14842', 0.6666)
+    area = frame.GetArea(x, y)
+    area.InsertFalseMullion(type.DefaultFalseMullion, True)
+    frame.InsertSashes()
+    frame.InsertGlasspackets(glasspacket)
+    if ctx.InsertFittings:
+        for sash in frame.GetSashes():
+            args = sash.GetFittingsFindArgs()
+            fittingsTypeID = db.FindFittingsType(fittingsGroup, args)
+            if sash.ID == 3:
+                sash.InsertFittings(fittingsTypeID, True)
+            else:
+                sash.InsertFittings(fittingsTypeID)";
         }
     }
 }
