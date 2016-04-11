ebe44ea1007abc4f7de3592a71f1c621aeb2a2a5
Tomas Kordik <kordik@winkhaus.cz>
2016-04-11 11:46:48 +0200

    Ctor: šedé obrázky na disablovaných buttonech

---
+++ Ctor/Views/AutoGreyImage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Ctor.Views
+{
+    public class AutoGreyImage : Image
+    {
+        static AutoGreyImage()
+        {
+            IsEnabledProperty.OverrideMetadata(typeof(AutoGreyImage), new FrameworkPropertyMetadata(true, new PropertyChangedCallback(OnAutoGreyScaleImageIsEnabledPropertyChanged)));
+        }
+
+        private static void OnAutoGreyScaleImageIsEnabledPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs args)
+        {
+            var autoGreyScaleImg = source as AutoGreyImage;
+            var isEnable = Convert.ToBoolean(args.NewValue);
+            if (autoGreyScaleImg != null)
+            {
+                if (!isEnable)
+                {
+                    // Get the source bitmap
+                    var bitmapImage = new BitmapImage(new Uri(autoGreyScaleImg.Source.ToString()));
+
+                    // Convert it to Gray
+                    autoGreyScaleImg.Source = new FormatConvertedBitmap(bitmapImage, PixelFormats.Gray32Float, null, 0);
+
+                    // Create Opacity Mask for greyscale image as FormatConvertedBitmap does not keep transparency info
+                    autoGreyScaleImg.OpacityMask = new ImageBrush(bitmapImage);
+                }
+                else
+                {
+                    // Set the Source property to the original value.
+                    autoGreyScaleImg.Source = ((FormatConvertedBitmap)autoGreyScaleImg.Source).Source;
+
+                    // Reset the Opacity Mask
+                    autoGreyScaleImg.OpacityMask = null;
+                }
+            }
+        }
+    }
+}
