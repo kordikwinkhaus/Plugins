904b360fa8da7464ad7da14f4dd884fa4b3ca8e1
Tomas Kordik <kordik@winkhaus.cz>
2016-05-03 15:20:49 +0200

    Ctor: ošetření sloupců v "locals" tree view

--- Ctor/Views/CodeEditorDialog.xaml.cs
+++ Ctor/Views/CodeEditorDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using Ctor.Models;
 using Ctor.Models.Scripting;
@@ -14,6 +15,12 @@ namespace Ctor.Views
         {
             InitializeComponent();
             codeEditor.TextArea.Caret.PositionChanged += new EventHandler(Caret_PositionChanged);
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            UpdateColumnsWidth(tlvLocals);
         }
 
         void Caret_PositionChanged(object sender, EventArgs e)
@@ -57,6 +64,27 @@ namespace Ctor.Views
                 MessageBox.Show(Strings.RunningScript, Msg.CAPTION, MessageBoxButton.OK, MessageBoxImage.Error);
                 e.Cancel = true;
             }
+        }
+
+        private void TreeListView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateColumnsWidth(tlvLocals);
+        }
+
+        private void UpdateColumnsWidth(TreeListView treeListView)
+        {
+            int[] ratios = new int[] { 1, 2, 1 };
+            double total = ratios.Sum();
+            double availableSpace = treeListView.ActualWidth;
+            double pxPerRatio = availableSpace / total;
+
+            for (int i = 0; i < ratios.Length - 1; i++)
+            {
+                double colWidth = ratios[i] * pxPerRatio;
+                availableSpace -= colWidth;
+                treeListView.Columns[i].Width = colWidth;
+            }
+            treeListView.Columns.Last().Width = availableSpace;
         }
     }
 }
