3028db523d591c2e379a61726d17f689c619ac0e
Tomas Kordik <kordik@winkhaus.cz>
2016-11-24 09:47:16 +0100

    Imager: nový plugin pro uložení obrázku

---
+++ Imager/ViewModels/ImagerViewModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+using Okna.Plugins;
+using Okna.Plugins.ViewModels;
+using WHOkna;
+
+namespace Imager.ViewModels
+{
+    public class ImagerViewModel : ViewModelBase
+    {
+        public ImagerViewModel()
+        {
+            this.SaveImageCommand = new RelayCommand(SaveImage);
+
+            this.Width = 120;
+            this.Height = 120;
+            this.DirectoryPath = GetDirectoryPath();
+        }
+
+        private static string GetDirectoryPath()
+        {
+            string filePath = new Uri(Assembly.GetAssembly(typeof(ImagerViewModel)).CodeBase).LocalPath;
+            return System.IO.Path.Combine(System.IO.Path.GetDirectoryName(filePath), "Scripts");
+        }
+
+        private uint _width;
+        public uint Width
+        {
+            get { return _width; }
+            set
+            {
+                if (_width != value)
+                {
+                    _width = value;
+                    OnPropertyChanged(nameof(Width));
+                }
+            }
+        }
+
+        private uint _height;
+        public uint Height
+        {
+            get { return _height; }
+            set
+            {
+                if (_height != value)
+                {
+                    _height = value;
+                    OnPropertyChanged(nameof(Height));
+                }
+            }
+        }
+
+        private string _directoryPath;
+        public string DirectoryPath
+        {
+            get { return _directoryPath; }
+            set
+            {
+                if (_directoryPath != value)
+                {
+                    _directoryPath = value;
+                    OnPropertyChanged(nameof(DirectoryPath));
+                }
+            }
+        }
+
+        private string _filename;
+        public string Filename
+        {
+            get { return _filename; }
+            set
+            {
+                if (_filename != value)
+                {
+                    _filename = value;
+                    OnPropertyChanged(nameof(Filename));
+                }
+            }
+        }
+
+        public IPart Part { get; internal set; }
+
+        public ICommand SaveImageCommand { get; private set; }
+
+        private void SaveImage(object param)
+        {
+            if (this.Part != null)
+            {
+                try
+                {
+                    CreateAndSaveImage(this.Part);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Imager", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void CreateAndSaveImage(IPart part)
+        {
+            part.Update(true); // PG: Constant height handles were not moved to its positions.
+            IDrawing drawing = part.GetDrawing();
+
+            drawing.Emf = false;
+            drawing.Colors = part.Position.Document.Application.ColorsOnScreen;
+            //drawing.DimsFlags = 1; // main dimensions
+            drawing.DimsFlags = 0; // no dimensions
+            drawing.PenStyle = 0;
+            drawing.FontName = "Arial";
+            drawing.FontSize = 10;
+            drawing.Height = this.Height;
+            drawing.Labels = false;
+            drawing.Links = true;
+            drawing.PenWidth = 1;
+            drawing.Width = this.Width;
+            drawing.Proportions = true;
+            if (drawing.Update())
+            {
+                var ms = drawing.Picture as MemoryStream;
+                ms.Position = 0;
+
+                File.WriteAllBytes(System.IO.Path.Combine(this.DirectoryPath, this.Filename + ".png"), ms.GetBuffer());
+            }
+        }
+    }
+}
---
+++ Imager/Views/ImagerPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Linq;
+using Imager.ViewModels;
+using UserExtensions;
+using WHOkna;
+
+namespace Imager.Views
+{
+    public partial class ImagerPage : UserControl, IUserForm3
+    {
+        private readonly ImagerViewModel _viewmodel;
+
+        public ImagerPage()
+        {
+            InitializeComponent();
+
+            this.DataContext = _viewmodel = new ImagerViewModel();
+        }
+
+        public IDataRequest DataCallback { get; set; }
+
+        public XElement ObjectData { get; set; }
+
+        public IOknaDocument OknaDoc { get; set; }
+
+        private IPart _part;
+        public IPart Part
+        {
+            get { return _part; }
+            set
+            {
+                _part = value;
+                _viewmodel.Part = value;
+            }
+        }
+
+        public object Title
+        {
+            get { return "Imager"; }
+        }
+
+        public bool SetData(XElement data, int document, int position, int profileType)
+        {
+            return true;
+        }
+
+        private void UserControl_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}
