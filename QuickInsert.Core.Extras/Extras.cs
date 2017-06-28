e56cb80a0a3ea560776799ad71b4132a645e117f
Tomas Kordik <kordik@winkhaus.cz>
2017-06-28 13:54:23 +0200

    Extras: příkladový plugin pro Karla - rozšíření QuickInsert

---
+++ QuickInsert.Core.Extras/Extras.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+using QuickInsert.Models;
+using WHOkna;
+
+namespace QuickInsert.Core
+{
+    public static class Extras
+    {
+        public static void VlozitOkop(Sash sash, string nrArt)
+        {
+            int colorID = sash.Color;
+
+            var okopParams = Parameters.ForMullion(nrArt, colorID);
+            var emptyArea = sash.GetEmptyArea();
+            var insertionPoint = new PointF((emptyArea.Left + emptyArea.Right) / 2, (emptyArea.Top + emptyArea.Bottom) / 2);
+            var area = (IArea)(emptyArea.Data);
+            var result = area.AddBar(EProfileType.tNakladka, EDir.dLeft, insertionPoint, okopParams);
+            area.TopObject.Update("Okop se nepodařilo vložit.");
+
+            if (result != null)
+            {
+                var bar = (IBar)result[0];
+                bar.SlidedToEdge = EDir.dBottom;
+                bar.TopObject.Update("Okop nelze zarovnat.");
+            }
+            else
+            {
+                throw new ModelException("Okop se nepodařilo vložit.");
+            }
+        }
+    }
+}
